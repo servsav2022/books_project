d29e31b85c7124d32d2fa1675dd728cf7d1d38a1
alexandr <svaluhinav@yandex.ru>
2024-01-07 14:06:20 +0500

    осталось обновление ролей и запись действий, остальное готово2

--- src/main/java/com/servsav/books_project/BooksProjectApplication.java
+++ src/main/java/com/servsav/books_project/BooksProjectApplication.java
@@ -1,9 +1,8 @@
 package com.servsav.books_project;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@Slf4j
+
 @SpringBootApplication
 public class BooksProjectApplication {
 
